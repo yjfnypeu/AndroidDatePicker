715596a8d30ffb1fdd0fd74f28546b1deabb0afe
yjfnypeu <470368500@qq.com>
2015-12-22 13:23:38 +0800

    upload date picker demo

--- app/src/main/java/org/lzh/androiddatepicker/MainActivity.java
+++ app/src/main/java/org/lzh/androiddatepicker/MainActivity.java
@@ -2,6 +2,14 @@ package org.lzh.androiddatepicker;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import org.lzh.datepickerlib.dtpicker.DTPicker;
+import org.lzh.datepickerlib.dtpicker.DTPickerDialog;
+
+import java.util.Calendar;
 
 public class MainActivity extends FragmentActivity {
 
@@ -9,5 +17,25 @@ public class MainActivity extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.go_dt_picker_dialog).setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                goSelectTime();
+            }
+        });
+    }
+
+    private void goSelectTime() {
+        DTPickerDialog dialog = new DTPickerDialog();
+        dialog.setDTSelectListener(new DTPicker.OnDTSelectListener() {
+
+            @Override
+            public void onDTSelected(Calendar calendar) {
+                Log.d("DTPicker", calendar.getTime().toString());
+                Toast.makeText(MainActivity.this, calendar.getTime().toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+        dialog.show(getSupportFragmentManager(), "DTPickerDialog");
     }
 }
